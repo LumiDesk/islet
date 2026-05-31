9a570ba6f88b5f8ff87a9fbecfd1818bfa2e9640
Talyra42 <talyra42@foxmail.com>
2026-05-31 10:39:10 +0800

    feat: 增加短路径支持和clockStore

--- src/components/MainClock.vue
+++ src/components/MainClock.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import { ref, onMounted, onUnmounted } from "vue";
+import { useClockStore } from "@/store/clock";
+
+const clockStore = useClockStore();
 
 const currentTime = ref("");
 let timer: ReturnType<typeof setTimeout>;
@@ -7,12 +10,15 @@ let timer: ReturnType<typeof setTimeout>;
 const updateTime = () => {
   const now = new Date();
 
-  // 补0
   const hours = String(now.getHours()).padStart(2, "0");
   const minutes = String(now.getMinutes()).padStart(2, "0");
-  const seconds = String(now.getSeconds()).padStart(2, "0");
 
-  currentTime.value = `${hours} : ${minutes} : ${seconds}`;
+  if (clockStore.showSeconds) {
+    const seconds = String(now.getSeconds()).padStart(2, "0");
+    currentTime.value = `${hours} : ${minutes} : ${seconds}`;
+  } else {
+    currentTime.value = `${hours} : ${minutes}`;
+  }
 
   const msUntilNextSecond = 1000 - now.getMilliseconds();
 
---
+++ src/store/clock.ts
@@ -0,0 +1,13 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+export const useClockStore = defineStore("islet-clock", () => {
+  /**
+   * 是否展示秒
+   */
+  const showSeconds = ref(false);
+
+  return {
+    showSeconds,
+  };
+});
